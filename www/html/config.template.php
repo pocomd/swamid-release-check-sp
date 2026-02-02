db3b0cc4063faa690468715c005f4fef96fcb2b4
Björn Mattsson <bjorn@sunet.se>
2026-02-02 14:19:17 +0100

    Updated config.template

--- www/html/config.template.php
+++ www/html/config.template.php
@@ -22,6 +22,13 @@ $federation = array(
   'adminUsers' => array('adminuser1@federation.org', 'adminuser2@federation.org',
     'user1@inst1.org', 'user1@inst2.org'),
 
+  # Urls could be relative or absolute depending on if they are hosted on the same box or not.
+  'aboutURL' => 'https://edugain.org/about-edugain/what-is-edugain/',
+  'contactURL' => 'https://edugain.org/contact/',
+  'logoURL' => 'https://edugain.org/wp-content/uploads/2017/06/header_logo_small-1.gif',
+  'logoWidth' => 163,
+  'logoHeight' => 40,
+
   # Optional if you want to extend HTML and TestSuite with an extended version
   # See TestSuiteSWAMID and HTMLSWAMID for examples
   #'extend' => 'SWAMID',
@@ -42,53 +49,29 @@ $federation = array(
   #'reuseSession' => true,
 
   'instructionsAttributes' => '<p>Click on the green button to see what attributes your Identity Provider releases.</p>
-          <p>Description of all test avaiable in the ' . $basename . ' identity federation test suite:
+          <p>Description of all test available in the eduGAIN test suite:
             <ul>
               <li>The Attributes tab shows all attributes the service release to the entityId https://release-check.<org>.<tld>/shibboleth. The entityId uses the entity categories:<ul>
                 <li>REFEDS Personalized Access Entity Category,</li>
                 <li>REFEDS Research and Scholarship Entity Category, and</li>
-                <li>REFEDS Data Protection Code of Conduct ver 2.0 Entity Category including all
-                  <a href="https://wiki.sunet.se/display/SWAMID/Entity+Category+attribute+release+in+SWAMID">
-                  ' . $basename . ' Best Practice attributes</a>.
-                </li>
+                <li>REFEDS Data Protection Code of Conduct ver 2.0 Entity Category.</li>
               </ul></li>
               <li>The Entity category tab does an exetensive testing of that an Identity Provider follows
-                ' . $basename . ' Best Practice attribute release via entity categories.</li>
+                Best Practice for attribute release via entity categories.</li>
               <li>The MFA tab checks if an Identity Provider is correctly configured for handling request
-                for multi-factor login as expected by SWAMID.</li>
+                for multi-factor login.</li>
               <li>The ESI tab verifies if the Identity Provider release the right attributes for the
                 European Digital Student Service Infrastructure.</li>
             </ul>
           </p>',
-    'instructionsEntityCategory' => '<p>In order for ' . $basename . ' to work as effectively as possible for students and employees as well as for
-            service providers and identity providers, ' . $basename . ' recommends that service providers use
+  'instructionsEntityCategory' => '<p>In order for eduGAIN to work as effectively as possible for students and employees as well as for
+            service providers and identity providers, eduGAIN recommends that service providers use
             entity categories to get the attributes that they require.</p>
-          <p>In order for services within the ' . $basename . ' federation to work as effectively as possible, ' . $basename . ' recommends
+          <p>In order for services within the eduGAIN federation to work as effectively as possible, eduGAIN recommends
             the use of entity categories. Entity categories benefits not only students and employees but also
             administrators of relying and identity providers by providing a
             stable framework for the release of attributes.</p>
-          <p>During autumn 2019, ' . $basename . ' has updated its entity category recommendations and these will be implemented
-            in our production environment during 2020 and 2021.</p>
-          <p>This service is designed to help administrators of identity providers verify that their
-            IdP follows the new recommendations.</p>
-          <p>SWAMID’s current recommendations for attribute release are available at
-            <a href="https://wiki.sunet.se/display/SWAMID/SAML+WebSSO+Service+Provider+Best+Current+Practice">
-              https://wiki.sunet.se/display/SWAMID/SAML+WebSSO+Service+Provider+Best+Current+Practice
-            </a>.
-          </p>
-          <p>Example configuration for Shibboleth can be found in the section entitled “Example of metadata
-            configuration, attribute resolvers and attribute filters” on the following wiki page
-            <a href="https://wiki.sunet.se/display/SWAMID/SAML+WebSSO+Identity+Provider+Best+Current+Practice">
-              https://wiki.sunet.se/display/SWAMID/SAML+WebSSO+Identity+Provider+Best+Current+Practice
-            </a>.
-          </p>
-          <p>The Swamid best practice attribute release check consists of the following tests:</p>',
-    'instructionsEntityCategoryEnd' => '<p>Multiple Code of Conduct test require different attributes which the IdP either SHOULD or SHOULD NOT
-            release in accordance REFEDS/GÉANT Code of Conduct.</p>
-          <p>For further information on how personal data is processed in SWAMID Best Practice Attribute Release
-            check see
-            <a href="https://wiki.sunet.se/display/SWAMID/SWAMID+Entity+Category+Release+Check+-+Privacy+Policy">
-              https://wiki.sunet.se/display/SWAMID/SWAMID+Entity+Category+Release+Check+-+Privacy+Policy
-            </a>
-          </p>',
+          <p>The eduGAIN best practice attribute release check consists of the following tests:</p>',
+  'instructionsEntityCategoryEnd' => '<p>Multiple Code of Conduct test require different attributes which the IdP either SHOULD or SHOULD NOT
+            release in accordance REFEDS/GÉANT Code of Conduct.</p>',
 );
