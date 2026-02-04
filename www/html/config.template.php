e49227878e3eaee6e3756a3e1724f112376507c9
Björn Mattsson <bjorn@sunet.se>
2026-02-04 23:25:44 +0100

    fix: Update Configuration and config.template.php with default values

--- www/html/config.template.php
+++ www/html/config.template.php
@@ -48,30 +48,35 @@ $federation = array(
   # true or false
   #'reuseSession' => true,
 
-  'instructionsAttributes' => '<p>Click on the green button to see what attributes your Identity Provider releases.</p>
-          <p>Description of all test available in the eduGAIN test suite:
-            <ul>
-              <li>The Attributes tab shows all attributes the service release to the entityId https://release-check.<org>.<tld>/shibboleth. The entityId uses the entity categories:<ul>
-                <li>REFEDS Personalized Access Entity Category,</li>
-                <li>REFEDS Research and Scholarship Entity Category, and</li>
-                <li>REFEDS Data Protection Code of Conduct ver 2.0 Entity Category.</li>
-              </ul></li>
-              <li>The Entity category tab does an exetensive testing of that an Identity Provider follows
-                Best Practice for attribute release via entity categories.</li>
-              <li>The MFA tab checks if an Identity Provider is correctly configured for handling request
-                for multi-factor login.</li>
-              <li>The ESI tab verifies if the Identity Provider release the right attributes for the
-                European Digital Student Service Infrastructure.</li>
-            </ul>
-          </p>',
-  'instructionsEntityCategory' => '<p>In order for eduGAIN to work as effectively as possible for students and employees as well as for
-            service providers and identity providers, eduGAIN recommends that service providers use
-            entity categories to get the attributes that they require.</p>
-          <p>In order for services within the eduGAIN federation to work as effectively as possible, eduGAIN recommends
-            the use of entity categories. Entity categories benefits not only students and employees but also
-            administrators of relying and identity providers by providing a
-            stable framework for the release of attributes.</p>
-          <p>The eduGAIN best practice attribute release check consists of the following tests:</p>',
-  'instructionsEntityCategoryEnd' => '<p>Multiple Code of Conduct test require different attributes which the IdP either SHOULD or SHOULD NOT
-            release in accordance REFEDS/GÉANT Code of Conduct.</p>',
+  # Optional instructions at Attributes tab. Default text below
+  #'instructionsAttributes' => '<p>Click on the green button to see what attributes your Identity Provider releases.</p>
+  #        <p>Description of all test available in the eduGAIN test suite:
+  #          <ul>
+  #            <li>The Attributes tab shows all attributes the service release to the entityId https://release-check.<org>.<tld>/shibboleth. The entityId uses the entity categories:<ul>
+  #              <li>REFEDS Personalized Access Entity Category,</li>
+  #              <li>REFEDS Research and Scholarship Entity Category, and</li>
+  #              <li>REFEDS Data Protection Code of Conduct ver 2.0 Entity Category.</li>
+  #            </ul></li>
+  #            <li>The Entity category tab does an exetensive testing of that an Identity Provider follows
+  #              Best Practice for attribute release via entity categories.</li>
+  #            <li>The MFA tab checks if an Identity Provider is correctly configured for handling request
+  #              for multi-factor login.</li>
+  #            <li>The ESI tab verifies if the Identity Provider release the right attributes for the
+  #              European Digital Student Service Infrastructure.</li>
+  #          </ul>
+  #        </p>',
+
+  # Optional instructions at EntityCategory tab. Default text below
+  #'instructionsEntityCategory' => '<p>In order for eduGAIN to work as effectively as possible for students and employees as well as for
+  #          service providers and identity providers, eduGAIN recommends that service providers use
+  #          entity categories to get the attributes that they require.</p>
+  #        <p>In order for services within the eduGAIN federation to work as effectively as possible, eduGAIN recommends
+  #          the use of entity categories. Entity categories benefits not only students and employees but also
+  #          administrators of relying and identity providers by providing a
+  #          stable framework for the release of attributes.</p>
+  #        <p>The eduGAIN best practice attribute release check consists of the following tests:</p>',
+
+  # Optional instructions at end of EntityCategory tab. Default text below
+  #'instructionsEntityCategoryEnd' => '<p>Multiple Code of Conduct test require different attributes which the IdP either SHOULD or SHOULD NOT
+  #          release in accordance REFEDS/GÉANT Code of Conduct.</p>',
 );
