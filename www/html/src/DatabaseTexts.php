e7d1d0730d4e70132b2a5e6a8eeb8c3b295e1203
Björn Mattsson <bjorn@sunet.se>
2026-03-31 14:37:52 +0200

    Added text-strings that are stored in Db that need to be translated

---
+++ www/html/src/DatabaseTexts.php
@@ -0,0 +1,81 @@
+<?php
+#IdPCheck.php:292:
+_('Did not send any attributes that were not requested.');
+#IdPCheck.php:577:
+_('R&S attributes OK, Entity Category Support OK');
+#IdPCheck.php:579:
+_('R&S attributes OK, Entity Category Support missing');
+#IdPCheck.php:587:
+_('R&S attributes missing, BUT Entity Category Support claimed');
+#IdPCheck.php:590:
+_('R&S attribute missing, Entity Category Support missing');
+
+#IdPCheck.php:620:
+_('Anonymous attributes OK, Entity Category Support OK');
+#IdPCheck.php:622:
+_('Anonymous attributes OK, Entity Category Support missing');
+#IdPCheck.php:630:
+_('Anonymous attributes missing, BUT Entity Category Support claimed');
+#IdPCheck.php:633:
+_('Anonymous attribute missing, Entity Category Support missing');
+
+#IdPCheck.php:696:
+_('Pseudonymous attributes OK, Entity Category Support OK');
+#IdPCheck.php:698:
+_('Pseudonymous attributes OK, Entity Category Support missing');
+#IdPCheck.php:705:
+_('Pseudonymous attributes missing, BUT Entity Category Support claimed');
+#IdPCheck.php:708:
+_('Pseudonymous attribute missing, Entity Category Support missing');
+
+#IdPCheck.php:782:
+_('Personalized attributes OK, Entity Category Support OK');
+#IdPCheck.php:784:
+_('Personalized attributes OK, Entity Category Support missing');
+#IdPCheck.php:791:
+_('Personalized attributes missing, BUT Entity Category Support claimed');
+#IdPCheck.php:794:
+_('Personalized attribute missing, Entity Category Support missing');
+
+#IdPCheck.php:834:
+_('CoCo OK, Entity Category Support OK');
+#IdPCheck.php:836:
+_('CoCo OK, Entity Category Support missing');
+#IdPCheck.php:843:
+_('CoCo is not supported, BUT Entity Category Support is claimed');
+#IdPCheck.php:846:
+_('Support for CoCo missing, Entity Category Support missing');
+
+#IdPCheck.php:864:
+#IdPCheck.php:885:
+_('schacPersonalUniqueCode OK');
+#IdPCheck.php:870:
+_('schacPersonalUniqueCode OK. BUT wrong case');
+#IdPCheck.php:874:
+_('schacPersonalUniqueCode not starting with urn:schac:personalUniqueCode:int:esi:');
+#IdPCheck.php:881:
+_('More than one schacPersonalUniqueCode');
+#IdPCheck.php:888:
+_('Missing schacPersonalUniqueCode');
+
+#IdPCheck.php:1002:
+_('Sends invalid Assurance information.');
+#IdPCheck.php:1005:
+_('Missing https://refeds.org/assurance for user.');
+#IdPCheck.php:1008:
+_('Missing some Assurance information.');
+#IdPCheck.php:1011:
+_('Sends recommended Assurance information.');
+
+#IdPCheck.php:1080:
+_('Supports REFEDS MFA and ForceAuthn.');
+#IdPCheck.php:1083:
+_('Supports REFEDS MFA but not ForceAuthn.');
+#IdPCheck.php:1086:
+_('Supports REFEDS MFA.');
+#IdPCheck.php:1091:
+_('Does support ForceAuthn but not REFEDS MFA.');
+#IdPCheck.php:1094:
+_('Does neither support REFEDS MFA or ForceAuthn.');
+#IdPCheck.php:1094:
+_('Does not support REFEDS MFA.');
