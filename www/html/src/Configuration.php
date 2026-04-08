a77aea1fcb62614fa50b8ff5ea88ceb4d27d431d
Björn Mattsson <bjorn@sunet.se>
2026-04-08 11:20:18 +0200

    Added all texts to translate

--- www/html/src/Configuration.php
+++ www/html/src/Configuration.php
@@ -63,21 +63,21 @@ class Configuration {
       'DS' => 'service.seamlessaccess.org',
       'LoginURL' => 'Login',
       'instructionsAttributes' => '<p>' . _('Click on the green button to see what attributes your Identity Provider releases.') . '</p>
-          <p>' . _('Description of all test available in the eduGAIN test suite') . ':
+          <p>' . sprintf(_('Description of all test available in the %s test suite'), $federation['displayName']) . ':
             <ul>
-              <li>' . _('The Attributes tab shows all attributes the service release to the entityId https://release-check.<org>.<tld>/shibboleth. The entityId uses the entity categories') . ':<ul>
+              <li>' . sprintf(_('The Attributes tab shows all attributes the service release to the entityId https://%s/shibboleth. The entityId uses the entity categories'), $basename) . ':<ul>
                 <li>' . _('REFEDS Personalized Access Entity Category') . ',</li>
                 <li>' . _('REFEDS Research and Scholarship Entity Category, and') . '</li>
                 <li>' . _('REFEDS Data Protection Code of Conduct ver 2.0 Entity Category') . '.</li>
               </ul></li>
+              <li>' . _('The Authentication tab checks if an Identity Provider is correctly configured for handling request for different AuthnContextClassRef requests.') . '</li>
               <li>' . _('The Entity category tab does an exetensive testing of that an Identity Provider follows Best Practice for attribute release via entity categories.') . '</li>
-              <li>' . _('The MFA tab checks if an Identity Provider is correctly configured for handling request for multi-factor login.') . '</li>
               <li>' . _('The ESI tab verifies if the Identity Provider release the right attributes for the European Digital Student Service Infrastructure.') . '</li>
             </ul>
           </p>',
-      'instructionsEntityCategory' => '<p>' . _('In order for eduGAIN to work as effectively as possible for students and employees as well as for service providers and identity providers, eduGAIN recommends that service providers use entity categories to get the attributes that they require.') . '</p>
-          <p>' . _('In order for services within the eduGAIN federation to work as effectively as possible, eduGAIN recommends the use of entity categories. Entity categories benefits not only students and employees but also administrators of relying and identity providers by providing a stable framework for the release of attributes.') . '</p>
-          <p>' . _('The eduGAIN best practice attribute release check consists of the following tests') . ':</p>',
+      'instructionsEntityCategory' => '<p>' . sprintf(_('In order for %s to work as effectively as possible for students and employees as well as for service providers and identity providers, %s recommends that service providers use entity categories to get the attributes that they require.'), $federation['displayName'], $federation['displayName']) . '</p>
+          <p>' . sprintf(_('In order for services within the %s federation to work as effectively as possible, %s recommends the use of entity categories. Entity categories benefits not only students and employees but also administrators of relying and identity providers by providing a stable framework for the release of attributes.'), $federation['displayName'], $federation['displayName']) . '</p>
+          <p>' . sprintf(_('The %s best practice attribute release check consists of the following tests'), $federation['displayName']) . ':</p>',
       'instructionsEntityCategoryEnd' => '<p>' . sprintf(_('Multiple Code of Conduct test require different attributes which the IdP either SHOULD or SHOULD NOT release in accordance REFEDS/%s Code of Conduct.'), 'GÉANT') . '</p>',
     );
 
--- www/html/src/DatabaseTextsSWAMID.php
+++ www/html/src/DatabaseTextsSWAMID.php
@@ -1,15 +1,37 @@
 <?php
-#IdPCheckSWAMID.php:49:            $this->status['testResult'] = 'schacPersonalUniqueCode starting with urn:schac:personalUniqueCode:int:esi:se:';
+#IdPCheckSWAMID.php:48: #status_ERROR
+_('schacPersonalUniqueCode should not announce SE. Use ladok.se / eduid.se or &lt;sHO&gt;.se');
+#IdPCheckSWAMID.php:49: testResult
 _('schacPersonalUniqueCode starting with urn:schac:personalUniqueCode:int:esi:se:');
-#IdPCheckSWAMID.php:193:        $this->status['testResult'] = 'Assurance Profile missing. Sends Assurance information!';
+#IdPCheckSWAMID.php:60: #status_ERROR
+_('schacPersonalUniqueCode should start with urn:schac:personalUniqueCode:int:esi:');
+#IdPCheckSWAMID.php:192: #status_ERROR
+_('Identity Provider is not approved for any SWAMID Identity Assurance Profiles but sends Assurance information!.');
+#IdPCheckSWAMID.php:193: testResult
 _('Assurance Profile missing. Sends Assurance information!');
-#IdPCheckSWAMID.php:196:        $this->status['testResult'] = 'Assurance Profile missing.';
+#IdPCheckSWAMID.php:195: #status_ERROR
+_('Identity Provider is not approved for any SWAMID Identity Assurance Profiles.');
+#IdPCheckSWAMID.php:196: testResult
 _('Assurance Profile missing.');
-#IdPCheckSWAMID.php:235:        $this->status['testResult'] = 'Have Assurance Profile. Sends invalid Assurance information.';
+#IdPCheckSWAMID.php:234: #status_ERROR
+_('Identity Provider is sending invalid Assurance information.');
+#IdPCheckSWAMID.php:235: testResult
 _('Have Assurance Profile. Sends invalid Assurance information.');
-#IdPCheckSWAMID.php:239:        $this->status['testResult'] = 'Have Assurance Profile. Missing http://www.swamid.se/policy/assurance/al1 for user.'; # NOSONAR Should be http://
+#IdPCheckSWAMID.php:238: #status_ERROR
+_('Missing Assurance information. Expected at least http://www.swamid.se/policy/assurance/al1');
+#IdPCheckSWAMID.php:239: testResult
 _('Have Assurance Profile. Missing http://www.swamid.se/policy/assurance/al1 for user.');
-#IdPCheckSWAMID.php:243:        $this->status['testResult'] = 'Have Assurance Profile. Missing some Assurance information.';
+#IdPCheckSWAMID.php:243: testResult
 _('Have Assurance Profile. Missing some Assurance information.');
-#IdPCheckSWAMID.php:247:        $this->status['testResult'] = 'Have Assurance Profile. Sends recommended Assurance information.';
+#IdPCheckSWAMID.php:247: testResult
 _('Have Assurance Profile. Sends recommended Assurance information.');
+
+#IdPCheckSWAMID.php:233: status_OK
+_('Identity Provider is approved for at least one SWAMID Identity Assurance Profiles.');
+#IdPCheckSWAMID.php:237: status_OK
+_('Identity Provider is approved for at least one SWAMID Identity Assurance Profiles.');
+#IdPCheckSWAMID.php:241: status_OK
+_('Identity Provider is approved for at least one SWAMID Identity Assurance Profiles.');
+#IdPCheckSWAMID.php:245: status_OK
+#IdPCheckSWAMID.php:246: status_OK
+_("Identity Provider is approved for at least one SWAMID Identity Assurance Profiles' and attribute release for current user follows SWAMID's recommendations.");
